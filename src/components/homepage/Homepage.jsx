c85e233e55c2a5310d3219609a99892236c84055
MelroseDigital <Tenyce.Melrose@gmail.com>
2022-06-13 14:56:34 -0700

    Updated HomePage

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,8 +1,21 @@
-export default function Homepage(){
+import React from 'react';
+//import logo from './logo.svg';
+import './Homepage.css';
+    
+      
+      export default function HomePage() {
+        return (
+          <div className="HomePage">
+            <header className="Home">
+              <img src="http://localhost:3000/banner2.png" alt="Image" 
+                size="object-fit:cover" width="100%" height="100%"></img>
 
-  return(
-      <>
-          <h1>Welcome to Scoops</h1>
-      </>
-  );
-}
+               <h1> Your Pocket Radio. </h1>
+                <h1>Customize. Share. Enjoy. </h1>
+            </header>
+          </div>
+        );
+      }
+
+      // style="object-fit:cover;width:100%;height:100%;
+//https://testing2222-22.my.canva.site/videos/a8af8eb93052bb7c86bb206b28a459b2.gif
