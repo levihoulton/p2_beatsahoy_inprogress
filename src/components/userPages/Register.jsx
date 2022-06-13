ef3738429786e7c249841aa7d4040f7ef99b7f33
Catherine Claunch <catherine490@revature.net>
2022-06-13 16:45:17 -0500

    Added to the register component

--- src/components/userPages/Register.jsx
+++ src/components/userPages/Register.jsx
@@ -1,8 +1,71 @@
+import { useRef } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+
+
 export default function Register(){
+    const navigate = useNavigate();
+
+    const fnameInput = useRef();
+    const lnameInput = useRef();
+    const usernameInput = useRef();
+    const passwordInput = useRef();
+    
+
+
+   
+
+    const url = "azureurl"
+
+    async function userReg(){
+        
+
+        const user = {
+            
+            fname: fnameInput.current.value,
+            lname: lnameInput.current.value,
+            username: usernameInput.current.value,
+            password: passwordInput.current.value,
+           
+        }
+       
+
+        
+        try{
+        const response = await axios.post(`${url}/register` , user)
+        navigate("/");
+       
+        console.log(response.data);
+        //console.log(response)
+        } catch(error){
+            console.error(error.response.data);
+                alert(error.response.data);
+        }
+        
+    }
 
     return(
         <>
-            <h1>Welcome to Scoops</h1>
+        
+        <center>
+        <br></br>
+        <br></br>
+        <br></br>
+        <input placeholder="Enter First Name" ref={fnameInput}></input>
+        <br></br>
+        <input placeholder="Enter Last Name" ref={lnameInput}></input>
+        <br></br>
+        <input placeholder="Enter Username" ref={usernameInput}></input>
+        <br></br>
+        <input type="password" placeholder="Enter password" ref={passwordInput}></input>
+        
+        <br></br>
+        <br></br>
+        <button onClick={userReg}>Register</button>
+
+</center>
         </>
-    );
+    )
+
 }
