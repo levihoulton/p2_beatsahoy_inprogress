c1f53f9f543c09a1eef7d0e15bfe0525446e57ff
MelroseDigital <Tenyce.Melrose@gmail.com>
2022-06-14 06:17:44 -0700

    HomePage,Buttons/Images/Util

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,21 +1,28 @@
+import { Button } from '@mui/material';
 import React from 'react';
-//import logo from './logo.svg';
+import { Navigate } from 'react-router-dom';
 import './Homepage.css';
-    
-      
+import Register from '../userPages/Register';  
+
+
+     
       export default function HomePage() {
         return (
+          <>
           <div className="HomePage">
             <header className="Home">
-              <img src="http://localhost:3000/banner2.png" alt="Image" 
+              <img src="http://localhost:3000/banner2.png" alt="Background" 
                 size="object-fit:cover" width="100%" height="100%"></img>
-
-               <h1> Your Pocket Radio. </h1>
-                <h1>Customize. Share. Enjoy. </h1>
+                <h1> Your Pocket Radio. </h1>
+                <h2>Customize. Share. Enjoy. </h2>
             </header>
           </div>
+        <Button
+  variant='contained'
+  sx={{ color: 'white', backgroundColor: 'black', borderColor: 'grey' }} onClick={ () => Navigate("/Register")}
+>
+     Start Listening     
+</Button>
+        </>
         );
       }
-
-      // style="object-fit:cover;width:100%;height:100%;
-//https://testing2222-22.my.canva.site/videos/a8af8eb93052bb7c86bb206b28a459b2.gif
