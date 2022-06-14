f14c31c7bfe969df734d89210aee136bcb719d89
Veronica Leslie <v.leslie423@gmail.com>
2022-06-14 18:42:58 -0400

    got the survey outlined and persisting to the console next we will add the spotify api

--- src/components/questions/Questions.jsx
+++ src/components/questions/Questions.jsx
@@ -1,121 +1,106 @@
-import React, {Component} from "react";
-import "survey-react/survey.css";
-import * as Survey from "survey-react";
+import axios from "axios";
+import { useRef } from "react";
 
-class Questionnaire extends Component{
-  constructor(props){
-    super(props)
-    this.state = {
+export default function Quiz(props) {
 
+    
+    const url = "https://beatsahoyp2.azurewebsites.net";
+
+    const energyInput = useRef();
+    const activityInput = useRef();
+    const weatherInput = useRef();
+    const vibeInput = useRef();
+    
+    // async-await
+    async function createPlaylist() {
+        // Whenever you are getting a useRefs value, make sure it's inside some function call. Otherwise it will
+        // error due to the refInput.current = undefined, meaning there is no .value available
+        const user = {
+            energy: energyInput.current.value,
+            activity: activityInput.current.value,
+            weather: weatherInput.current.value,
+            vibe: vibeInput.current.value,
+            
+        };
+        console.log(user);
+        try {
+            const response = await axios.post(`${url}/playlists`, user);
+            console.log(response.data);
+        } catch (error) {
+            console.error(error.response.data);
+            alert(error.response.data);
+        }
+        window.location.reload(false);
     }
-    this.onCompleteComponent = this.onCompleteComponent.bind(this)
-  }
-  onCompleteComponent = () =>{
-    this.setState({
-      isCompleted: true
-    })
-  }
-render() {
-  var json = {
-    elements: [
-      {
-        type: "dropdown",
-        name: "energy",
-        title: "what is you current energy level?",
-        isRequired: true,
-        choices: [
-          "Low/Sleepy",
-          "relaxed/chill",
-          "pretty energetic",
-          "PUMPED/super high energy"
-
-
-        ]
-        
-      },
-      
-      {
-        type: "panel",
-        name: "panel",
-       
-        elements: [
-          {
-            type: "dropdown",
-            name: "activoty",
-            title: "What are you doing now?",
-            isRequired: true,
-            choices: [
-              "Winding down for the night",
-              "Just waking up/ starting the day",
-              "Getting some exercise",
-              "Partying"
-              
-            ]
-          },
+  
+    return (
+     
+        <div >
+          <h1>Question</h1>
           
-           {
-            type: "dropdown",
-            name: "weather",
-            title: "What's the weather like right now?",
-            isRequired: true,
-            choices: [
-                "snowy",
-                " heavy rain",
-                "cloudy",
-                "nice and sunny"
-
-
-            ]
         
-              
-            },
-            {
-                type: "dropdown",
-                name: "vibe",
-                title: "What vibe are you going for?",
-                isRequired: true,
-                choices: [
-                    "sad/gloomy",
-                    "in love",
-                    "happy",
-                    "energized",
-                    "angry"
-                ]
-            }
-          
-        ]
-      }
-    ]
-  };
-    var surveyRender = !this.state.isCompleted ? (
-      <Survey.Survey
-      json={json}
-      showCompletedPage={false}
-      onComplete={this.onCompleteComponent}
-    />
-    ) : null
+        <hr/>
+  
+        <div>
+          <p>{props.question1}</p>
+          <p>what is your current energy level?</p>
+  
+          <div >
+            <input type="radio" name="energy" ref={energyInput} value= "1" />Low/Sleepy
+            <input type="radio" name="energy" ref={energyInput} value= "3" />relaxed/chill
+            <input type="radio" name="energy" ref={energyInput} value= "6" />pretty energetic
+            <input type="radio" name="energy" ref={energyInput} value= "9" />PUMPED/super high energy
 
-    var onSurveyCompletion = this.state.isCompleted ? (
-        <button  >Click to view your personalized playlist!</button>
-    ) : null;
-  return (
+            <div>
+          <p>{props.question2}</p>
+          <p>What are you doing now</p>
+  <div>
+    <input type="radio" name="activity" ref={activityInput} value= "1"/>Winding down for the night
+    <input type="radio" name="activity" ref={activityInput} value= "4" />Just waking up/ starting the day
+    <input type="radio" name="activity" ref={activityInput} value= "8" />Getting some exercise
+    <input type="radio" name="activity" ref={activityInput} value= "10" />Partying
+    
+    </div>
+  </div>
+  <div >
+          <p>{props.question3}</p>
+  <p>What's the weather like right now</p>
+  <div >
+    <input type="radio" name="weather" ref={weatherInput} value= "3"/>snowy
+    <input type="radio" name="weather" ref={weatherInput} value= "2" />heavy rain
+    <input type="radio" name="weather" ref={weatherInput} value= "1" />cloudy
+    <input type="radio" name="weather" ref={weatherInput} value= "9" />nice and sunny
+    
+    </div>
+  </div>
+  <div >
+          <p>{props.question4}</p>
+  <p>What vibe are you going for?</p>
+  <div >
+    <input type="radio" name="vibe" ref={vibeInput} value= "1" />sad/gloomy
+    <input type="radio" name="vibe" ref={vibeInput} value= "4" />romantic
+    <input type="radio" name="vibe" ref={vibeInput} value= "8" />happy
+    <input type="radio" name="vibe" ref={vibeInput} value= "9" />energized
+    <input type="radio" name="vibe" ref={vibeInput} value= "6" />angry
+    
+    </div>
+  </div>
+        </div>
    
+            
 
-  <>
-        <h1>Welcome to BeatsAhoy</h1>
-
-
-<h2>Complete the short questionnaire below to maximize your playlist experience</h2>
-
-<div className="Questionnaire">
-      
-      {surveyRender}
-      {onSurveyCompletion}
-     </div>
         
-        </>)
-}
-}
-
-export default  Questionnaire;
-//create questionnaire where you input 1-10 then connect those inputs times ten to the spotify api to set the parameters for the types of song added to the playlist
+          </div>
+          <button onClick={createPlaylist}>Submit</button>
+          </div>
+         
+  
+ 
+          
+        
+        
+      
+      
+    );
+  }
+  
---
+++ src/components/questions/quiz.js
@@ -0,0 +1,7 @@
+<Quiz
+  question="1. Which one is not a JavaScript variable?"
+  option1="var a=5"
+  option2="let b=6"
+  option3="const c=3"
+  option4="int x=9"
+/>
