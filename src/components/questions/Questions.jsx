eadcaaa4aba30326a46f18e428a02b7aee4d2447
levihoulton <62920325+levihoulton@users.noreply.github.com>
2022-06-21 20:53:38 -0500

    Update Questions.jsx

--- src/components/questions/Questions.jsx
+++ src/components/questions/Questions.jsx
@@ -269,6 +269,7 @@ export default function Quiz(props) {
           }else{
             prompt("Please submit a playlist name before loading songs")
           }
+      addPlaylist()
       
   }
     return (
